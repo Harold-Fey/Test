b425fa9effab00c0e95939896b41adb069113f87
Harold FAFOLHAN <harold@dmdconsult.com>
2022-11-07 17:59:17 +0100

    first commit

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Tests\Support\C;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;    
+use App\Http\Controllers\PassportAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+Route::group(['middleware'=>['auth:api']],function(){
+    Route::post('logout', [PassportAuthController::class, 'logout']);    
 });
+
+
+Route::middleware('auth:api')->group(function() {
+    Route::resource('posts', PostController::class);
+
+});
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
